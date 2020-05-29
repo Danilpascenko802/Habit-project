eb4ea3a35a0d22718553a37c1d6ea2f2731a4b00
bigfish12 <2023felixs@psi.kiev.ua>
2020-05-29 23:52:03 +0300

    Eventlistener added

---
+++ src/Felix.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const buttonForHabit = document.querySelector("#habit-button-id");
+
+
+// function addHabit() {
+//     alert("Test")
+// }
+
+// buttonForHabit.addEventListener("click", addHabit);
+buttonForHabit.addEventListener("click", addNewHabitSection);
